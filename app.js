ebb7bccee704c46b15669496b65538f8166bbf20
Raisa Zaman <raisazaman2000@gmail.com>
2023-06-13 14:24:00 -0400

    finish event binding

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+  methods: {
+    addNumber(num) {
+      return (this.counter = this.counter + num);
+    },
+    reduceNumber(num) {
+      return (this.counter = this.counter - num);
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    submitForm() {
+      alert("submitted");
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
